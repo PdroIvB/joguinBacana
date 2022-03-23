c19e7a9239705a859c7e74248ec0889ecacde5df
Adamilton <adapereira54@gmail.com>
2022-03-23 16:48:08 -0300

    drag and drop code initially

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+$(".item").draggable({
+    containment : "body"
+})
+
+$( ".local-img" ).droppable({
+    drop:function(event,ui){
+        console.log("drop");
+        const localImg = $(this).attr("type-obj");
+    
+        const typeObj = ui.draggable.attr("type-obj");
+        if(typeObj === localImg){
+            console.log("accertou");
+            $(`#${localImg}`).css({"top": "-51px", "left": "148px"});
+           /*  console.log($(`#${localImg}`).css("top"))    
+            console.log($(`#${localImg}`).css("left")) */
+
+        }else{
+
+        }
+
+        //$("#score").html(score);
+        ui.draggable.attr("inside",1);
+    },
+    over: function (event, ui) {
+        if(Number(ui.draggable.attr("inside"))){
+            //score -= Number(ui.draggable.attr("value"));
+            //$("#score").html(score);
+        }
+    },
+    out: function (event, ui) {
+        //ui.draggable.attr("inside",0)
+    }
+});
+
+
+
+
 function startTimer(duration, display) {
     let timer = duration
     let interval = 
@@ -14,32 +51,32 @@ function startTimer(duration, display) {
     }, 1000);
     
 }
-function animatedTimer(timer,duration){
-    console.log(timer)
-    if(timer == duration - (duration / 10) * 1){
-        document.getElementById('t10').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 2){
-        document.getElementById('t9').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 3){
-        document.getElementById('t8').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 4){
-        document.getElementById('t7').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 5){
-        document.getElementById('t6').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 6){
-        document.getElementById('t5').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 7){
-        document.getElementById('t4').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 8){
-        document.getElementById('t3').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 9){
-        document.getElementById('t2').setAttribute("style", "background-color:white")
-    }else if(timer == duration - (duration / 10) * 10){
-        document.getElementById('t1').setAttribute("style", "background-color:white")
+    /* function animatedTimer(timer,duration){
+        console.log(timer)
+        if(timer == duration - (duration / 10) * 1){
+            document.getElementById('t10').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 2){
+            document.getElementById('t9').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 3){
+            document.getElementById('t8').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 4){
+            document.getElementById('t7').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 5){
+            document.getElementById('t6').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 6){
+            document.getElementById('t5').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 7){
+            document.getElementById('t4').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 8){
+            document.getElementById('t3').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 9){
+            document.getElementById('t2').setAttribute("style", "background-color:white")
+        }else if(timer == duration - (duration / 10) * 10){
+            document.getElementById('t1').setAttribute("style", "background-color:white")
+        }
     }
-}
-window.onload = function () {
-    const duration = 60 * 2; 
-    display = document.getElementById('timer');
-    startTimer(duration, display); 
-};
+    window.onload = function () {
+        const duration = 60 * 2; 
+        display = document.getElementById('timer');
+        startTimer(duration, display); 
+    }; */
