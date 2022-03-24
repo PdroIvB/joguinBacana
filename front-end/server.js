23deb494f748c58b9635afb6aa853261008ac338
Victor Campos <victorhscampos@gmail.com>
2022-03-24 12:50:27 -0300

    definindo pasta tela inicial para o static

--- front-end/server.js
+++ front-end/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = 6969;
 
-app.use(express.static(__dirname + "/jogao"));
+app.use(express.static(__dirname + "/jogao/tela_inicial"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-//FIXME qqr coisa
+
