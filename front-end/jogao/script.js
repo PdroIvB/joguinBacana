a3a9f5acb186b9e6a8dfb8aef61fbd91973b6961
Pedro Ivo <pedroicbarbos@gmail.com>
2022-03-25 13:32:28 -0300

    pagina inicial aparecendo no link

---
+++ front-end/jogao/script.js
@@ -0,0 +1,11 @@
+function entrar_jogo(){
+    window.location = "/primeira_fase"
+}
+
+function entrar_instrucao(){
+    window.location = "/tela_instrucao"
+}
+
+function voltar_tela_jogo(){
+    window.location = "/tela_ranking"
+}
